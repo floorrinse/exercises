c3e9d68786b555cfb4da3f6798903b9bd1933ec8
florence <florencecodestoo@gmail.com>
2021-12-10 20:59:09 -0500

    feat: 7 kyu add description

--- codewars/7-get-list-sum-recursively.js
+++ codewars/7-get-list-sum-recursively.js
@@ -1,3 +1,7 @@
+/* 
+Write function sumR which returns the sum of values in a given list. Try no to cheat and provide recursive solution.
+*/
+
 function sumR(x) {
   if (x.length === 0) {
     return 0;
