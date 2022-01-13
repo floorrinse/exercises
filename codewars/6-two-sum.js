11ff1f0dc2c9e5336dcb5724ae67332ad629d67f
florence <florencecodestoo@gmail.com>
2022-01-12 21:41:07 -0500

    remove

--- codewars/6-two-sum.js
+++ codewars/6-two-sum.js
@@ -1,5 +1,5 @@
 /* 
-6 kyu Two Sum
+
 
 Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
 
