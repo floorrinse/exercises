2286248c915863d3e6acf0281287d08c2f64f5d1
florence <florencecodestoo@gmail.com>
2022-01-19 17:33:34 -0500

    add description

--- codewars/6-if-you-can-read-this.js
+++ codewars/6-if-you-can-read-this.js
@@ -1,5 +1,16 @@
 /* 
 6 kyu If you can read this...
+
+Task
+You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+Input:
+
+If, you can read?
+
+Output:
+
+India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
 */
 
 function to_nato(words) {
