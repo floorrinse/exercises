e90d742910559e295c821bcfbec95f6f663045c0
florence <florencecodestoo@gmail.com>
2022-01-19 17:32:45 -0500

    fix: title

--- codewars/6-if-you-can-read-this.js
+++ codewars/6-if-you-can-read-this.js
@@ -1,3 +1,7 @@
+/* 
+6 kyu If you can read this...
+*/
+
 function to_nato(words) {
   return words.split('').filter(word => word !== ' ').map(letter => NATO[letter.toLowerCase()] || letter).join(' ');
 }
