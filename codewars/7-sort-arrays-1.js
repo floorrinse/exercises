ea1fb7b3ed4f67ca9f5a4da15508bcf9c9c3086c
florence <florencecodestoo@gmail.com>
2022-02-10 13:49:18 -0500

    feat: 7 kyu

---
+++ codewars/7-sort-arrays-1.js
@@ -0,0 +1,10 @@
+/* 
+7 kyu Sort arrays - 1
+
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+sortme = function (names) {
+  return names.sort()
+}
+
