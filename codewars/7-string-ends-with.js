42635ef06365c9569c5d21de6ac3b7d0ebba34c2
florence <florencecodestoo@gmail.com>
2022-03-17 23:25:58 -0400

    add description

--- codewars/7-string-ends-with.js
+++ codewars/7-string-ends-with.js
@@ -1,5 +1,7 @@
 /* 
 7 kyu String ends with?
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 */
 
 function solution(str, ending) {
