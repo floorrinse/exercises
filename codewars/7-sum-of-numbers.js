19908abf8161ccc8ecacff18069a425705d8cf2f
florence <florencecodestoo@gmail.com>
2022-03-23 15:44:58 -0400

    feat: sum of numbers

---
+++ codewars/7-sum-of-numbers.js
@@ -0,0 +1,37 @@
+/* 
+
+7 kyu Beginner Series #3 Sum of Numbers
+
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples(a,b) --> output(explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+*/
+
+function GetSum(a, b) {
+    // 1. if a === b, return a;
+    // 2. convert a,b to array and sort it 
+    //sort(a, b) => (a-b)
+    // 3a. create sum var
+    // 3. for loop 
+    // 4. sum += i, return sum;
+    if (a === b) {
+        return a;
+    }
+    else {
+        let sum = 0;
+        let sortedArray = Array.of(a, b).sort((a, b) => a - b);
+        for (i = sortedArray[0]; i <= sortedArray[sortedArray.length - 1]; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
