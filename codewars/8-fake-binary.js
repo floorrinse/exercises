1d8a2d1dace3be9da29a7ac2a7fa40a05cc5817a
florence <florencecodestoo@gmail.com>
2022-04-01 22:40:26 -0400

    spacing

--- codewars/8-fake-binary.js
+++ codewars/8-fake-binary.js
@@ -2,8 +2,6 @@
 8 kyu Fake Binary
 
 Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
-
-
 */
 
 function fakeBin(x) {
