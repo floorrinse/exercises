d1a7b9f1d44113165afe6abcfaa961abf74cdc30
florence <florencecodestoo@gmail.com>
2022-04-15 23:05:17 -0400

    add title

--- codewars/7-sPoNgEbOb MeMe.js
+++ codewars/7-sPoNgEbOb MeMe.js
@@ -1,4 +1,6 @@
-/* */
+/* 
+7 kyu sPoNgEbOb MeMe
+*/
 
 function spongeMeme(sentence) {
   return sentence.toLowerCase().split('').map((letter, index) => index % 2 === 0 ? letter.toUpperCase() : letter).join('');
