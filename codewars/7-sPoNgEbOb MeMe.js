1b221ec2455fc98faecbfd3a3c459472bf388be9
florence <florencecodestoo@gmail.com>
2022-04-18 23:06:20 -0400

    add description

--- codewars/7-sPoNgEbOb MeMe.js
+++ codewars/7-sPoNgEbOb MeMe.js
@@ -1,5 +1,11 @@
 /* 
 7 kyu sPoNgEbOb MeMe
+
+Remember the spongebob meme that is meant to make fun of people by repeating what they say in a mocking way?
+
+You need to create a function that converts the input into this format, with the output being the same string expect there is a pattern of uppercase and lowercase letters.
+
+
 */
 
 function spongeMeme(sentence) {
