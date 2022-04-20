5b996c47f8801810189035661ec2fd70deb30d3b
florence <florencecodestoo@gmail.com>
2022-04-19 23:08:00 -0400

    add test examples

--- codewars/7-sPoNgEbOb MeMe.js
+++ codewars/7-sPoNgEbOb MeMe.js
@@ -5,7 +5,10 @@ Remember the spongebob meme that is meant to make fun of people by repeating wha
 
 You need to create a function that converts the input into this format, with the output being the same string expect there is a pattern of uppercase and lowercase letters.
 
+Example:
 
+input:  "stop Making spongebob Memes!"
+output: "StOp mAkInG SpOnGeBoB MeMeS!"
 */
 
 function spongeMeme(sentence) {
