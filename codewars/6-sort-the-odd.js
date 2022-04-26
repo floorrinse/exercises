6d793a1d6fb246bb98a6a766d129ce4f99f73fae
florence <florencecodestoo@gmail.com>
2022-04-26 16:45:53 -0400

    add description

--- codewars/6-sort-the-odd.js
+++ codewars/6-sort-the-odd.js
@@ -1,6 +1,10 @@
 /*
 6 kyu Sort the odd
- */
+
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+
+*/
 
 function sortArray(array) {
   let odds = array.filter(num => num % 2 !== 0).sort((a, b) => a - b);
