9d9f4475049a7480aaa5f525cd73673e9a501e7d
florence <florencecodestoo@gmail.com>
2022-04-29 15:53:38 -0400

    shortest word

---
+++ codewars/7-shortest-word.js
@@ -0,0 +1,11 @@
+/*
+7 kyu Shortest Word
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s) {
+  return s.split(' ').sort((a, b) => a.length - b.length).shift().length;
+}
