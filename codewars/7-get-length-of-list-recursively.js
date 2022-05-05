99b6ac531b19e79ff6696c4719afad3d5937447e
florence <florencecodestoo@gmail.com>
2022-05-05 16:52:31 -0400

    add description

--- codewars/7-get-length-of-list-recursively.js
+++ codewars/7-get-length-of-list-recursively.js
@@ -1,6 +1,7 @@
 /* 
 7 kyu Get length of the list recursively
 
+Write funcion lenR which returns the length of a given list. Try no to cheat and provide recursive solution.
 */
 
 function lenR(x) {
