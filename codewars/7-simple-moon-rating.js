99d6c520801db8299593521826e6c2b615bed94c
florence <florencecodestoo@gmail.com>
2022-06-22 23:38:30 -0400

    feat: add moon rating

---
+++ codewars/7-simple-moon-rating.js
@@ -0,0 +1,11 @@
+function moonRating(rating) {
+  let moons = (rating % 1) < 0.5 ? Math.floor(rating) / 2 : Math.ceil(rating) / 2;
+
+  if (moons % 1 === 0) {
+    return 'o'.repeat(moons) + 'x'.repeat(5 - moons);
+  }
+  else {
+    let wholeMoons = Math.floor(moons);
+    return 'o'.repeat(wholeMoons) + 'c'.repeat(1) + 'x'.repeat(4 - wholeMoons);
+  }
+}
